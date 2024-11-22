35b3a6731d2a8a0e37ac61588a333c7568843ad3
Artem Polivanov <polivanov.a@yandex.ru>
2024-11-22 09:28:29 +0300

    Add NearestStationsService

---
+++ Schedule/CommonObjects/Constants.swift
@@ -0,0 +1,5 @@
+import Foundation
+
+struct Constants {
+    static let apiKey = "be5a3a46-9b76-4afb-b919-b16cf32da5ff"
+}
--- Schedule/ContentView.swift
+++ Schedule/ContentView.swift
@@ -1,4 +1,5 @@
 import SwiftUI
+import OpenAPIURLSession
 
 struct ContentView: View {
     var body: some View {
@@ -9,6 +10,31 @@ struct ContentView: View {
             Text("Hello, world!")
         }
         .padding()
+        .onAppear {
+            do { try stations() } catch {
+                print("Failed to load Stations: \(error.localizedDescription)")
+            }
+        }
+    }
+}
+
+extension ContentView {
+    func stations() throws {
+        let client = Client(
+            serverURL: try Servers.Server1.url(),
+            transport: URLSessionTransport()
+        )
+        
+        let service = NearestStationsService(client: client)
+        
+        Task {
+            let stations = try await service.getNearestStations(
+                lat: 59.864177,
+                lng: 30.319163,
+                distance: 50
+            )
+            print("Successfully loaded Stations (\(stations.stations?.count ?? 0))")
+        }
     }
 }
 
