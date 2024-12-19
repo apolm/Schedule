ef0f1e61b5c0fa708719ad6260a0fcfb545e2faf
Artem Polivanov <polivanov.a@yandex.ru>
2024-12-19 07:47:37 +0300

    Refactor files structure

--- Schedule/Views/Tabs/MainTabView.swift
+++ Schedule/Views/Tabs/MainTabView.swift
@@ -1,61 +1,23 @@
 import SwiftUI
 
 struct MainTabView: View {
-    @StateObject var searchViewModel = ScheduleSearchViewModel()
-    @State private var scheduleListViewModel: ScheduleListViewModel?
-    
     var body: some View {
         TabView {
-                VStack(spacing: 16) {
-                    ScheduleSearchView(viewModel: searchViewModel)
-                        .padding(.horizontal, 16)
-                    
-                    if let stationFrom = searchViewModel.stationFrom,
-                       let stationTo = searchViewModel.stationTo,
-                       let settlementFrom = searchViewModel.settlementFrom,
-                       let settlementTo = searchViewModel.settlementTo {
-                        Button {
-                            scheduleListViewModel = ScheduleListViewModel(
-                                settlementFrom: settlementFrom,
-                                stationFrom: stationFrom,
-                                settlementTo: settlementTo,
-                                stationTo: stationTo,
-                                dataProvider: MockDataProvider()
-                            )
-                        } label: {
-                            Text("Найти")
-                                .font(.body)
-                                .bold()
-                                .foregroundStyle(.ypWhiteUniversal)
-                                .padding(.horizontal, 47.5)
-                                .padding(.vertical, 20)
-                                .background(Color.ypBlue)
-                                .cornerRadius(16)
-                        }
-                    }
-                    
-                    Spacer()
+            ScheduleTabView()
+                .tabItem {
+                    Image(Assets.tabSchedule)
+                        .renderingMode(.template)
+                        .foregroundStyle(.ypBlack)
                 }
-                .padding(.top, 16)
-                .background(.ypWhite)
-            .tabItem {
-                Image(Assets.tabSchedule)
-                    .renderingMode(.template)
-                    .foregroundStyle(.ypBlack)
-                
-            }
             
             SettingsTabView()
-            .tabItem {
-                Image(Assets.tabSettings)
-                    .renderingMode(.template)
-                    .foregroundStyle(.ypBlack)
-            }
+                .tabItem {
+                    Image(Assets.tabSettings)
+                        .renderingMode(.template)
+                        .foregroundStyle(.ypBlack)
+                }
         }
         .tint(.ypBlack)
-        .fullScreenCover(item: $scheduleListViewModel) { scheduleListViewModel in
-            ScheduleListView(viewModel: scheduleListViewModel)
-        }
     }
 }
 
